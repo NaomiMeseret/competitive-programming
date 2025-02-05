85c903c74d37c4e4197250456bc7a13f93fe543d
NaomiMeseret <167538343+NaomiMeseret@users.noreply.github.com>
2025-02-05 19:10:33 +0300

    Created 'Shuffle String' by A2SV Hub

---
+++ 05-Feb-2025/Shuffle String 244459.py
@@ -0,0 +1,10 @@
+# Problem: Shuffle String - https://leetcode.com/problems/shuffle-string/description/
+
+class Solution:
+    def restoreString(self, s: str, indices: List[int]) -> str:
+        res=[""]*len(s)
+        for i in range(len(s)):
+            res[indices[i]]=s[i]
+        return "".join(res)
+
+        

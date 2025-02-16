715eedeed16655ed7edfd3ae7ffe01d1078b2d76
NaomiMeseret <167538343+NaomiMeseret@users.noreply.github.com>
2025-02-16 17:51:19 +0300

    Created '3Sum Closest ' by A2SV Hub

---
+++ 16-Feb-2025/3Sum Closest  267686.py
@@ -0,0 +1,19 @@
+# Problem: 3Sum Closest  - https://leetcode.com/problems/3sum-closest/
+
+def threeSumClosest(self, nums: List[int], target: int) -> int:
+        closestSum=float("inf")
+        nums.sort()
+        for i in range(len(nums)-2):
+            left=i+1
+            right=len(nums)-1
+            while left<right:
+                currentSum=nums[left]+nums[i]+nums[right]
+                if abs(currentSum - target) < abs(closestSum - target):
+                    closestSum = currentSum
+                if currentSum < target:
+                    left += 1
+                elif currentSum > target:
+                    right -= 1
+                else:
+                    return currentSum
+        return closestSum

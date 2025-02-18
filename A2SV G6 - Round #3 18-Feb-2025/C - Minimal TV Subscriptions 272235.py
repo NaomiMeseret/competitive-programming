6c3ff9528ce71efd31b886695d9f84b3c01e2562
NaomiMeseret <167538343+NaomiMeseret@users.noreply.github.com>
2025-02-18 19:12:26 +0300

    Created 'C - Minimal TV Subscriptions' by A2SV Hub

---
+++ A2SV G6 - Round #3 18-Feb-2025/C - Minimal TV Subscriptions 272235.py
@@ -0,0 +1,17 @@
+# Problem: C - Minimal TV Subscriptions - https://codeforces.com/gym/588468/problem/C
+
+from collections import Counter
+t=int(input())
+for _ in range(t):
+    n,k,d=map(int,input().split())
+    a=list(map(int,input().split()))
+    window=Counter(a[:d])
+    minLength=len(window)
+    l=0
+    for i in range(d,len(a)):
+        window[a[i]]+=1
+        window[a[i-d]]-=1
+        if window[a[i-d]]==0:
+            del window[a[i-d]]
+        minLength=min(minLength,len(window))
+    print(minLength)

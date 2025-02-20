21ee441237c57728bf2228b26346b62dc4b142ea
NaomiMeseret <167538343+NaomiMeseret@users.noreply.github.com>
2025-02-20 06:22:58 +0300

    Created 'B - Anansi and Trip-Photographs' by A2SV Hub

---
+++ A2SV G6 - Round #3 20-Feb-2025/B - Anansi and Trip-Photographs 275429.py
@@ -0,0 +1,14 @@
+# Problem: B - Anansi and Trip-Photographs - https://codeforces.com/gym/588468/problem/B
+
+t=int(input())
+for _ in range(t):
+    n,x=map(int,input().split())
+    h=list(map(int,input().split()))
+    h.sort()
+    front=h[:n]
+    back=h[n:]
+    res=all(back[i]-front[i]>=x for i in range(n))
+    if res:
+        print("YES")
+    else:
+        print("NO")

5e59b9a383daeefd6ac2bc08ce1d52a000e67a94
NaomiMeseret <167538343+NaomiMeseret@users.noreply.github.com>
2025-02-23 17:33:13 +0300

    Created 'Alternating Subsequence' by A2SV Hub

---
+++ 23-Feb-2025/Alternating Subsequence 281127.py
@@ -0,0 +1,18 @@
+# Problem: Alternating Subsequence - https://codeforces.com/contest/1343/problem/C
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    arr = list(map(int, input().split()))
+    totalSum = 0
+    currentMax = arr[0]
+    
+    for i in range(1, n):
+        if (arr[i] > 0) == (currentMax > 0):
+            currentMax = max(currentMax, arr[i])
+        else:
+            totalSum += currentMax
+            currentMax = arr[i]
+    
+    totalSum += currentMax
+    print(totalSum)

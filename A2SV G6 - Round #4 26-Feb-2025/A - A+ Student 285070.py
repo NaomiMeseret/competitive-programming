4d838c3480e58e68b64c39b99ae7ea8f992aabed
NaomiMeseret <167538343+NaomiMeseret@users.noreply.github.com>
2025-02-26 18:55:01 +0300

    Created 'A - A+ Student' by A2SV Hub

---
+++ A2SV G6 - Round #4 26-Feb-2025/A - A+ Student 285070.py
@@ -0,0 +1,18 @@
+# Problem: A - A+ Student - https://codeforces.com/gym/590053/problem/A
+
+t=int(input())
+for _ in range(t):
+    nums=list(map(int, input().split()))
+    maxNum=max(nums)
+    ans=[]
+    count=nums.count(maxNum)
+    for num in nums:
+        if len(set(nums))==1:
+            ans.append(1)
+        elif num==maxNum and count==1:
+            ans.append(0)
+        elif num==maxNum and count>0:
+            ans.append(1)
+        else:
+            ans.append(maxNum-num+1)
+    print(" ".join(map(str,ans)))

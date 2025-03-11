9a6243ae2c902069144158b67e213a39d95843b8
NaomiMeseret <167538343+NaomiMeseret@users.noreply.github.com>
2025-03-11 16:42:06 +0300

    Created 'D - Repeating Cipher' by A2SV Hub

---
+++ A2SV G6 - Round #1 11-Mar-2025/D - Repeating Cipher 302525.py
@@ -0,0 +1,12 @@
+# Problem: D - Repeating Cipher - https://codeforces.com/gym/585107/problem/D
+
+n=int(input())
+s=input()
+res=[]
+i = 0
+step = 1
+while i < n:
+    res.append (s[i])
+    i += step
+    step += 1
+print("".join(res))

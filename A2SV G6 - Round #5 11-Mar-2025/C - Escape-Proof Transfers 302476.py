59cee741a915cd1c3f3c404443b33b8f058d39e4
NaomiMeseret <167538343+NaomiMeseret@users.noreply.github.com>
2025-03-11 16:05:02 +0300

    Created 'C - Escape-Proof Transfers' by A2SV Hub

---
+++ A2SV G6 - Round #5 11-Mar-2025/C - Escape-Proof Transfers 302476.py
@@ -0,0 +1,12 @@
+# Problem: C - Escape-Proof Transfers - https://codeforces.com/gym/591928/problem/C
+
+n, t, c = map(int, input().split())
+p = list(map(int, input().split()))
+l = 0  
+count = 0  
+for r in range(n): 
+    if p[r] > t:
+        l = r + 1   
+    elif r - l + 1 >= c:
+        count += 1  
+print(count)

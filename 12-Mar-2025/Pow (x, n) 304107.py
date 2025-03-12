8b759003a092f04b1508d82b7254721d2c2371cc
NaomiMeseret <167538343+NaomiMeseret@users.noreply.github.com>
2025-03-12 18:29:30 +0300

    Created 'Pow (x, n)' by A2SV Hub

---
+++ 12-Mar-2025/Pow (x, n) 304107.py
@@ -0,0 +1,19 @@
+# Problem: Pow (x, n) - https://leetcode.com/problems/powx-n/
+
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        def power(x,n):
+            if n==0:
+                return 1
+            return x* pow(x,n-1)
+        return(power(x,n))
+      
+
+
+
+
+
+
+
+
+        

da30274a3bacbbb4a5087116674b9a5077a9ac7a
NaomiMeseret <167538343+NaomiMeseret@users.noreply.github.com>
2025-03-23 23:21:25 +0300

    Created 'Same Tree' by A2SV Hub

---
+++ 23-Mar-2025/Same Tree 316356.py
@@ -0,0 +1,24 @@
+# Problem: Same Tree - https://leetcode.com/problems/same-tree/description/
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        queueP = deque([p])
+        queueQ = deque([q])
+        while queueP and queueQ:
+            nodeP = queueP.popleft()
+            nodeQ = queueQ.popleft()
+            if not nodeP and not nodeQ:
+                continue
+            if not nodeP or not nodeQ or nodeP.val!=nodeQ.val:
+                return False
+            queueP.append(nodeP.left)
+            queueP.append(nodeP.right)
+            queueQ.append(nodeQ.left)
+            queueQ.append(nodeQ.right)
+        return not queueP and not queueQ
